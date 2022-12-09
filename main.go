49a32744139de6cee6e63e28e88a678fbeea6c7d
Daniel Bahrami <daniel@bahrami.dk>
2022-12-09 23:56:33 +0100

    Handle all default branch names

--- main.go
+++ main.go
@@ -20,18 +20,11 @@ func main() {
 	fmt.Scanln(&repository)
 
 	url := "https://github.com/" + username + "/" + repository
-	zipUrl := url + "/archive/master.zip"
+	apiUrl := "https://api.github.com/repos/" + username + "/" + repository
 
-	zipFile, err := os.Create(repository + ".zip")
+	request, err := http.Get(apiUrl)
 	if err != nil {
-		fmt.Printf("Failed to create zip file: %v\n", err)
-		os.Exit(1)
-	}
-	defer zipFile.Close()
-
-	request, err := http.Get(zipUrl)
-	if err != nil {
-		fmt.Printf("Failed to download zip file: %v\n", err)
+		fmt.Printf("Failed to retrieve repository information: %v\n", err)
 		os.Exit(1)
 	}
 	defer request.Body.Close()
@@ -40,6 +33,29 @@ func main() {
 		fmt.Printf("Repository \"%s/%s\" is either private or does not exist\n", username, repository)
 		os.Exit(1)
 	}
+
+	var repoInfo Repository
+	err = json.NewDecoder(request.Body).Decode(&repoInfo)
+	if err != nil {
+		fmt.Printf("Failed to parse repository information: %v\n", err)
+		os.Exit(1)
+	}
+
+	zipUrl := url + "/archive/" + repoInfo.DefaultBranch + ".zip"
+
+	zipFile, err := os.Create(repository + ".zip")
+	if err != nil {
+		fmt.Printf("Failed to create zip file: %v\n", err)
+		os.Exit(1)
+	}
+	defer zipFile.Close()
+
+	request, err = http.Get(zipUrl)
+	if err != nil {
+		fmt.Printf("Failed to download zip file: %v\n", err)
+		os.Exit(1)
+	}
+	defer request.Body.Close()
 
 	_, err = io.Copy(zipFile, request.Body)
 	if err != nil {
