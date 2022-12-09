79a8877ca4f5bba1c606c4c721cb439e5148e517
Daniel Bahrami <daniel@bahrami.dk>
2022-12-09 21:30:38 +0100

    Retrieve repository information

--- main.go
+++ main.go
@@ -8,5 +8,12 @@ import (
 )
 
 func main() {
+	var username, repository string
+	fmt.Print("Enter GitHub username: ")
+	fmt.Scanln(&username)
+	fmt.Print("Enter repository name: ")
+	fmt.Scanln(&repository)
 
+	url := "https://github.com/" + username + "/" + repository
+	zipUrl := url + "/archive/master.zip"
 }
